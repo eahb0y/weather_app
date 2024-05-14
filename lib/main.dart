ceff5dd0ad14392e4955c986034acbeb120c01bc
eahb0y <iskandarmister323@gmail.com>
2024-05-14 21:32:35 +0500

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'constants/features/home/presentation/pages/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return HomePage();
+  }
+}
+
+
+
